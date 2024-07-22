dd48c13b984218e77a29e6f37d20057f89c3ce70
Mik3007 <michelealtieri3007@gmail.com>
2024-07-22 18:20:46 +0200

    sistemato il footer in schermo piccolo

--- frontend/src/components/MyFooter.jsx
+++ frontend/src/components/MyFooter.jsx
@@ -3,9 +3,9 @@ import { Footer, FooterCopyright, FooterLink, FooterLinkGroup } from "flowbite-r
 export default function MyFooter() {
   return (
     <Footer className="bg-[#153448] dark:bg-black h-24">
-      <div className="container mx-auto flex justify-between items-center h-full px-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center h-full px-4">
         <FooterCopyright href="#" by="The Blog App" year={2022} />
-        <FooterLinkGroup className="flex space-x-4">
+        <FooterLinkGroup className="flex mb-2 md:space-y-0 md:space-x-4">
           <FooterLink href="#">About</FooterLink>
           <FooterLink href="#">Privacy Policy</FooterLink>
           <FooterLink href="#">Licensing</FooterLink>
